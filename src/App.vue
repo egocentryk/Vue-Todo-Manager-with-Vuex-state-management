a7a2f3275cc890e8594fae91733956f71d58ed4b
Łukasz <kontakt@restyled.pl>
2019-07-31 17:18:39 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="container">
+          <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+
+          <AddTodo />
+          <FilterTodos />
+          <Todos />
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import AddTodo from './components/AddTodo'
+    import Todos from './components/Todos'
+    import FilterTodos from './components/FilterTodos'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            AddTodo,
+            Todos,
+            FilterTodos
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        margin-top: 30px;
+    }
+
+    .container {
+        max-width: 1100px;
+        margin: auto;
+        overflow: auto;
+        padding: 0 2rem;
+    }
+
+    .logo {
+        max-width: 30px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto 
+    }
 </style>
